e5d1579b5ace8170f8fd28f4bb08ea0b66916dce
hat021048 <73167096+hat021048@users.noreply.github.com>
2021-05-05 23:15:55 +0200

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,41 @@
 input.onButtonPressed(Button.A, function () {
-    Periode += -5000
+    Periode += -1000
 })
+function Stopp () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+}
+function Bak () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.analogWritePin(AnalogPin.P1, Math.constrain(Speed, 100, 1023))
+    Regulator()
+}
+function Fram () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.analogWritePin(AnalogPin.P0, Math.constrain(Speed, 100, 1023))
+    Regulator()
+}
 input.onButtonPressed(Button.B, function () {
-    Periode += 5000
+    Periode += 1000
 })
+function Regulator () {
+    MåltPeriode = Math.constrain(pins.pulseIn(DigitalPin.P8, PulseValue.High), 15000, 25000)
+    Speed += Math.round((MåltPeriode - Periode) / Periode * 100)
+}
+let MåltPeriode = 0
+let Periode = 0
+let Speed = 0
 basic.showIcon(IconNames.Heart)
-let Speed = 200
-let Periode = 25000
-let MåltPeriode = Periode
+Speed = 200
+Periode = 25000
+MåltPeriode = Periode
 pins.digitalWritePin(DigitalPin.P1, 0)
 basic.forever(function () {
-    pins.analogWritePin(AnalogPin.P0, Math.constrain(Speed, 100, 1023))
-    MåltPeriode = pins.pulseIn(DigitalPin.P8, PulseValue.High)
-    Speed += Math.round((MåltPeriode - Periode) / Periode * 100)
+    if (pins.digitalReadPin(DigitalPin.P16) == 0) {
+        Fram()
+    } else {
+        for (let index = 0; index < 50; index++) {
+            Bak()
+        }
+    }
 })
